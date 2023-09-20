f6ddd5c3a1a1379980a7d5e5633db0fb0bd51f01
Shruti Gupta <202151151@iiitvadodara.ac.in>
2023-09-20 11:58:00 +0530

    BranchTrial2

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
